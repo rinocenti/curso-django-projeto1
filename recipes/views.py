bdea6830006800aea3c4747890f43aedfc075988
Ricardo Adriano Inocenti <adm@syspace.com.br>
2023-06-25 21:12:23 -0300

    Removido conta e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
